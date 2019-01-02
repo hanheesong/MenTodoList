f060f88eefd27268b426266a295447562ec9ff86
hanheesong <gmlthd0908@gmail.com>
2019-01-02 18:18:32 +0900

    feat: MEN - Building a Todo List App

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://heesong:gkslthd1@ds042908.mlab.com:42908/todolist', {
+    useNewUrlParser: true
+}).then(function() {
+    console.log('Database connected.');
+});
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extened: true }));
+
+const mustacheExpressInstance = mustacheExpress();
+mustacheExpressInstance.cache = null;
+app.engine('mustache', mustacheExpressInstance);
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use('/', routes);
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
